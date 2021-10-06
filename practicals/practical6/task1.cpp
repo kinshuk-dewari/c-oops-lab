5bb390bd8d5b465ed00f2646130124702dd92e1a
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-06 09:34:08 +0530

    Create task1.cpp

---
+++ practicals/practical6/task1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  float c,d;
+  string s1,s2;
+  cout<<"enter two int numbers :\n";
+  cin>>a>>b;
+  cout<<"enter two float numbers :\n";
+  cin>>c>>d;
+  cout<<"enter 1 string :\n";
+  getline(s1);
+  cout<<"enter 2 string :\n";
+  getline(s2);
+  /* int + int
+  float + float
+  float + int 
+  string + string 
+  string + int */
+  return 0;
+}
