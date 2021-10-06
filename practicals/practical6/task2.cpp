77d2b30b46923f87778f9b908ddbca67acaa4853
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-06 10:58:56 +0530

    Update task2.cpp

--- practicals/practical6/task2.cpp
+++ practicals/practical6/task2.cpp
@@ -1,15 +1,18 @@
-#include<bits/stdc++>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
 string str,str2;
 cout<<"enter a string :\n";
   getline(cin,str);
-  for(int i=str.end();i!=str.begin();i--){
-    str2+=*i;  
+  string :: iterator i;
+  for(i=str.end();i>=str.begin();i--){
+    str2 += *i;  
   }
-  if (str==str2)
-    cout<<"pallindrome\n";
+  cout<<str2<<"\n";
+  cout<<str;
+  if (str2==str)
+    cout<<"\npallindrome\n";
   else
-    cout<<"not a pallindrome\n";
+    cout<<"not a pallindrome\n"; 
   return 0;
 }
