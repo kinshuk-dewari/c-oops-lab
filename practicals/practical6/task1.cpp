a3c30815ab46e05254e872a9e32381f6d7a5e829
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-06 09:44:29 +0530

    Update task1.cpp

--- practicals/practical6/task1.cpp
+++ practicals/practical6/task1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdC++.h>//this library includes all the standard libraries.
 using namespace std;
 int main()
 {
@@ -10,13 +10,18 @@ int main()
   cout<<"enter two float numbers :\n";
   cin>>c>>d;
   cout<<"enter 1 string :\n";
-  getline(s1);
+  getline(cin,s1);
   cout<<"enter 2 string :\n";
-  getline(s2);
+  getline(cin,s2);
   /* int + int
   float + float
   float + int 
   string + string 
   string + int */
+  cout<<"int + int "<<(a+b)<<endl;
+  cout<<"float + float"<<(c+d)<<endl;
+  cout<<"float + int"<<(a+c)<<endl;
+  cout<<"sring + string"<<(s1+s2)<<endl;
+  cout"string + int "<<(a+s1)<<endl;
   return 0;
 }
