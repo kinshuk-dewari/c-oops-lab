d6def1350a312a96cd4b7373861ac2c8b5b4ffff
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-07 09:38:29 +0530

    Create task3.cpp

---
+++ practicals/practical6/task3.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++>
+using namespace std;
+int main(){
+string s;
+cout<<"enter a string :\n";
+getline(cin,s);
+  std string :: iterator i;
+for( i=s.end();i>=s.begin();i++){
+cout<<*i;} 
+}
