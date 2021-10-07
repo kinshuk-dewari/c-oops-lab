a8e79f867b6f3417b3eaa1146ca46aec430aa137
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-07 09:46:48 +0530

    Update task3.cpp

--- practicals/practical6/task3.cpp
+++ practicals/practical6/task3.cpp
@@ -5,6 +5,8 @@ string s;
 cout<<"enter a string :\n";
 getline(cin,s);
 string :: iterator i;
+cout<<"the reverse of the string is : ";
 for( i=s.end();i>=s.begin();i++){
-cout<<*i;} 
+cout<<*i;}
+  return 0;
 }
