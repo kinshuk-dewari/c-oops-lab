a4f759f1da1fe47d772d47f596306f1b241c94ab
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-13 10:45:54 +0530

    Update task1.cpp

--- practicals/practical7/task2/task1.cpp
+++ practicals/practical7/task2/task1.cpp
@@ -3,4 +3,29 @@
 2.print using -10
 3.same as 1 but restore in array
 4.same as 2 but restore in array */
+#include<iostream>
+using namespace std;
+int main()
+{
+  int m;
+  cout<<"enter the rows : \n";
+  cin>>m;
+  int a[m][m];
+  cout<<"enter the elements :\n";
+  for(int i=0;i<m;i++)
+  {
+    for(int j=0;j<m;j++)
+    {
+      cin>>a[i][j];
+    }
+  }
+  for(int i=0;i<m;i++)
+  {
+    for(int j=0;j<m;j++)
+    {
+      cout<<10-a[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
   
