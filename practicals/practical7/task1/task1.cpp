9fff4a3dbcebaba9e1dd459103fd8076a3032b5b
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-13 09:56:06 +0530

    Update task1.cpp

--- practicals/practical7/task1/task1.cpp
+++ practicals/practical7/task1/task1.cpp
@@ -4,4 +4,31 @@ using namespace std;
   2.print the same using inbuilt functions*/
 int main()
 {
+  int ch;
+  do{
+  cout<<"1.day\n2.date\n3.month\n4.year5.exit\n";
+  cout<<"enter your choice :\n";
+  cin>>ch; 
+  switch(ch)
+  {
+    case 1:
+      cout<<"wednesday\n";
+      break;
+    case 2:
+      cout<<"13\n";
+      break;
+    case 3:
+      cout<<"october\n";
+      break;
+    case 4:
+      cout<<"2021\n";
+      break;
+    case 5:
+      break;
+    default:
+      cout<<"wrong choice\n";
+      break;
+  }
+  }while(ch!=5)
+  return 0;
 }
