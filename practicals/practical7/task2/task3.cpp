94a3fc42b0ca650c8bab21d05dae3f929e76011d
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-17 22:13:26 +0530

    Create task3.cpp

---
+++ practicals/practical7/task2/task3.cpp
@@ -0,0 +1,48 @@
+//problem : only works if m = even, if m = odd mid row doesn't swap//
+#include<iostream>
+using namespace std;
+int main()
+{
+  int m;
+  cout<<"enter the rows : \n";
+  cin>>m;
+  int x=m-1;
+  int a[m][m];
+  cout<<"enter the elements :\n";
+  for(int i=0;i<m;i++)
+  {
+    for(int j=0;j<m;j++)
+    {
+      cin>>a[i][j];
+    }
+  }
+  cout<<"\n\nthe array is :\n\n";
+  for (int i=0;i<m/2;i++)
+  {
+      if (x>=0)
+      {
+      int y=m-1;
+      for (int j=0;j<m;j++)
+      {
+          
+          int temp=a[i][j];
+          a[i][j]=a[x][y];
+          a[x][y]=temp;
+          y--;
+      }
+      x--;
+      }
+      else
+        break;
+  }
+  for(int i=0;i<m;i++)
+  {
+    for(int j=0;j<m;j++)
+    {
+      cout<<a[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+ 
+}
+  
