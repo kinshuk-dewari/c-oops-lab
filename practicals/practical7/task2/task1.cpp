0aec2d03cd258b15cb49c1707fb2b3936111a9a1
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-17 20:56:25 +0530

    Update task1.cpp

--- practicals/practical7/task2/task1.cpp
+++ practicals/practical7/task2/task1.cpp
@@ -1,8 +1,3 @@
-/*print 2 d array
-1.pritnt value only 
-2.print using -10
-3.same as 1 but restore in array
-4.same as 2 but restore in array */
 #include<iostream>
 using namespace std;
 int main()
