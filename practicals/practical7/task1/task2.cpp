a35056dd5f90c44af2c5fbf0538b6b6d8add7497
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-17 20:53:05 +0530

    Update task2.cpp

--- practicals/practical7/task1/task2.cpp
+++ practicals/practical7/task1/task2.cpp
@@ -1,36 +1,12 @@
-#include<iostream>
 #include<ctime>
+#include<iostream>
 using namespace std;
 int main()
 {
-  int ch;
-  time_t now =time(0);
-  tm *ltm = localtime(&now);
-  char* dt = ctime(&now);
-  do{
-  cout<<"1.day\n2.date\n3.month\n4.year5.exit\n";
-  cout<<"enter your choice :\n";
-  cin>>ch; 
-  switch(ch)
-  {
-    case 1:
-      cout<<"Day : ",1 + ltm->tm_mon<< endl;
-      break;
-    case 2:
-      cout<<"Date : 13\n",dt<<endl;
-      break;
-    case 3:
-      cout<<"Month : ",1 + ltm->tm_mon<< endl;
-      break;
-    case 4:
-      cout<<"Year :",1900 + ltm->tm_year<<endl;
-      break;
-    case 5:
-      break;
-    default:
-      cout<<"wrong choice\n";
-      break;
-  }
-  }while(ch!=5)
-  return 0;
+  time_t=time(NULL);
+  tm* timePtr = localtime(&t);
+  cout<<"Day = "<<timePtr->tm_mday<<endl;
+  cout<<"Month = "<<timePtr->tm_mon<<endl;
+  cout<<"Year = "<<timePtr->tm_year<<endl;
+  cout<<"Day of year = "<<timePtr->tm_yday<<endl;
 }
