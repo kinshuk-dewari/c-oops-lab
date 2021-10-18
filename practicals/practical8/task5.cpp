7891ee581ac1f0e9711f43e2e343e5b7a411c2dc
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-18 10:27:31 +0530

    Add files via upload

---
+++ practicals/practical8/task5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int fact(int n)
+{
+	if(n>1)
+	{
+		return n*fact(n-1);
+	}
+	if(n==1||n==0)
+	{
+		return 1;
+	}
+}
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<fact(n);
+	return 0;
+}
