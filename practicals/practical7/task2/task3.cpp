25633eebf9475fefd0c6a8bc105c46997801b52b
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-18 10:56:01 +0530

    Update task3.cpp

--- practicals/practical7/task2/task3.cpp
+++ practicals/practical7/task2/task3.cpp
@@ -46,4 +46,43 @@ int main()
   }
  
 }
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cout << "Enter the order of the matrix : ";
+	cin >> N;
+	
+	int arr[N][N];
+	cout << "Enter the elements : " << endl;
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+			cin >> arr[i][j];
+	}
+	
+	int res = N % 2 == 0 ? N/2 - 1: N/2;    
+	for(int i = 0; i <= res; i++)
+	{
+	for(int j = 0; j < N; j++)
+	{    
+	    if (i == N-1-i && j == N-1-j)
+		break;
+	    int t = arr[i][j];
+	    arr[i][j] = arr[N-1-i][N-1-j];
+	    arr[N-1-i][N-1-j] = t;
+	}
+	}
+	
+	cout << "\nArray after reverse : " << endl;
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}       
+	return 0;
+} */
   
