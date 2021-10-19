ace4ef705d0a6dacfe860cbea6fd465958d7dcbd
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-20 00:07:25 +0530

    Update task2.cpp

--- practicals/practical7/task3/task2.cpp
+++ practicals/practical7/task3/task2.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int m;
+  cout<<"enter the rows : \n";
+  cin>>m;
+  int x=m-1;
+  int a[m][m];
+  cout<<"enter the elements :\n";
+  for(int i=0;i<m;i++)
+  {
+    for(int j=0;j<m;j++)
+    {
+      cin>>a[i][j];
+    }
+  }
+  cout<<"\n\nthe array is :\n\n";
 
+   for(int i=0;i<m;i++)
+  {
+    for(int j=0;j<m;j++)
+    {
+      cout<<i+1<<" ";
+    }
+     cout<<endl;
+  }
+    
+}
