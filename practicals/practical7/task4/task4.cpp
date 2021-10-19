dd08780a5c6f9a6916cc26f9fde163ad968cfd11
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-20 00:22:33 +0530

    Create task4.cpp

---
+++ practicals/practical7/task4/task4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int i,j;
+  string a[4][4];
+  for(i=0;i<4;i++)
+  {
+      for(j=0;j<4;j++)
+      {
+          if (i+j>3)
+            a[i][j]=" ";
+          else
+            a[i][j]="*";
+    }
+  }
+   for(i=0;i<4;i++)
+   {
+      for(j=0;j<4;j++)
+      {
+       cout<<a[i][j]<<" ";
+      }
+      cout<<endl;
+  }
+  return 0;
+}
