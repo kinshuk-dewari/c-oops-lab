ce4998dead42e5260f2f032396a3d2dc9e0fdeed
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-20 00:27:40 +0530

    Update task2.cpp

--- practicals/practical7/task4/task2.cpp
+++ practicals/practical7/task4/task2.cpp
@@ -1,53 +1,26 @@
 #include<iostream>
-
 using namespace std;
-
 int main()
-
 {
-
   int i,j;
-
   string a[4][4];
-
   for(i=0;i<4;i++)
-
   {
-
     for(j=0;j<4;j++)
-
     {
-
        if (j<i)
-
        a[i][j]=" ";
-
        else
-
-       a[i][j]="*";
-
-      
-
+       a[i][j]="*";  
     }
-
   }
-
    for(i=0;i<4;i++)
-
   {
-
     for(j=0;j<4;j++)
-
     {
-
       cout<<a[i][j]<<" ";
-
     }
-
      cout<<endl;
-
   }
-
   return 0;
-
 }
