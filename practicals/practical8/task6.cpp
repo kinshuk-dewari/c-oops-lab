b54fe26131b8f2e0792e1dd5de07b4138a3f0bd7
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-10-28 23:28:52 +0530

    Update task6.cpp

--- practicals/practical8/task6.cpp
+++ practicals/practical8/task6.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+int series(int n)
+{
+    if(n==2)
+    {
+        cout<<n<<" , ";
+        return pow(n,n)+n;
+    }
+    else
+    {
+        cout<<series(n-1)<<" , ";
+    }
+    return pow(n,n)+n;
+}
 int main()
 {
+    int n;
+    cout<<"Enter the limit : ";
+    cin>>n;
+    series(n+1);
+    cout<<endl;
+    return 0;
 }
