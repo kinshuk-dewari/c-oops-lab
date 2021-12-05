e037f78879cdb01ccf53c0b5d559d394bb36ea7f
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-12-05 12:45:05 +0530

    Update kk.cpp

--- prime with output/kk.cpp
+++ prime with output/kk.cpp
@@ -2,21 +2,26 @@
 #include<cmath>
 using namespace std;
 void prime(int num)
-{int i=0,ctr=1;
-for(i=2;i<=sqrt(num);i++){
-if(num%i==0){
-ctr=0;
-break;}}
-if(ctr==0){
-cout<<"not Prime";}
+{
+  int i=0,ctr=1;
+  for(i=2;i<=sqrt(num);i++)
+  {
+    if(num%i==0)
+    {
+      ctr=0;
+      break;
+    }
+  }
+if(ctr==0)
+  cout<<"not Prime";}
 else
-cout<<" prime";
+  cout<<" prime";
 }
 int main()
 {
-int ctr=1,i,num,s;
-cout<<"enter the number : \n";
-cin>> num;
-prime(num);
-return 0;
+  int ctr=1,i,num,s;
+  cout<<"enter the number : \n";
+  cin>> num;
+  prime(num);
+  return 0;
 }
